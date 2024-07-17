f7670a0d2b80cc75bbb8bbbf508bf36c75bd6143
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-07-17 15:51:15 +0000

    add code genatror

--- src/components/forms/codeGenerator/FormCodeGenerator.tsx
+++ src/components/forms/codeGenerator/FormCodeGenerator.tsx
@@ -1,20 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Button, Form, Input, Select, Checkbox, Radio, DatePicker, TimePicker } from "antd";
+import React, { useState } from "react"; 
 import useDesigner from "@/hooks/useDesigner";
+import { Button, Modal } from "antd";
 
-
-
-
- 
-
-const FormCodeGenerator = ( ) => {
+const FormCodeGenerator = () => {
   const [componentCode, setComponentCode] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-  const [isTemplate, setIsTemplate] = useState(false);
-  const [elementsTemplatePreview, setElementsTemplatePreview] = useState<FormElement[]>([]);
   const { elements, submitBtn } = useDesigner();
- 
+
   const generateComponentCode = () => {
     const componentCode = elements.map((input, index) => {
       let inputElement = "";
@@ -39,27 +31,47 @@ const FormCodeGenerator = ( ) => {
         case "select":
           inputElement = `
             <Select placeholder="${input.elementType.placeholder}" style={{ width: "100%" }}>
-              ${input.elementType.options?.map((option, idx) => `<Select.Option key={${idx}} value="${option}">${option}</Select.Option>`).join('\n')}
+              ${input.elementType.options
+                ?.map(
+                  (option, idx) =>
+                    `<Select.Option key={${idx}} value="${option}">${option}</Select.Option>`
+                )
+                .join("\n")}
             </Select>`;
           break;
         case "select_multiple":
           inputElement = `
             <Select mode="multiple" placeholder="${input.elementType.placeholder}" style={{ width: "100%" }}>
-              ${input.elementType.options?.map((option, idx) => `<Select.Option key={${idx}} value="${option}">${option}</Select.Option>`).join('\n')}
+              ${input.elementType.options
+                ?.map(
+                  (option, idx) =>
+                    `<Select.Option key={${idx}} value="${option}">${option}</Select.Option>`
+                )
+                .join("\n")}
             </Select>`;
           break;
         case "checkbox":
           inputElement = `
             <Checkbox.Group>
               <div className="flex flex-col space-y-2">
-                ${input.elementType.options?.map((option, idx) => `<Checkbox key={${idx}} value="${option}">${option}</Checkbox>`).join('\n')}
+                ${input.elementType.options
+                  ?.map(
+                    (option, idx) =>
+                      `<Checkbox key={${idx}} value="${option}">${option}</Checkbox>`
+                  )
+                  .join("\n")}
               </div>
             </Checkbox.Group>`;
           break;
         case "radio":
           inputElement = `
             <Radio.Group>
-              ${input.elementType.options?.map((option, idx) => `<div key={${idx}} className="flex items-center"><Radio value="${option}">${option}</Radio></div>`).join('\n')}
+              ${input.elementType.options
+                ?.map(
+                  (option, idx) =>
+                    `<div key={${idx}} className="flex items-center"><Radio value="${option}">${option}</Radio></div>`
+                )
+                .join("\n")}
             </Radio.Group>`;
           break;
         default:
@@ -76,20 +88,25 @@ const FormCodeGenerator = ( ) => {
               required: ${input.elementType.required},
               message: \`${input.elementType.label} is required\`,
             },
-            ${input.elementType.pattern
-              ? input.elementType.pattern.map((pattern, idx) => `{
+            ${
+              input.elementType.pattern
+                ? input.elementType.pattern
+                    .map(
+                      (pattern, idx) => `{
                 pattern: new RegExp("${pattern}"),
                 message: \`Please match the requested format for ${input.elementType.label}\`,
                 key: ${idx},
-              }`).join(', ')
-              : ''
+              }`
+                    )
+                    .join(", ")
+                : ""
             }
           ]}
         >
           ${inputElement}
         </Form.Item>`;
     });
-
+ 
     const exportCode = `
       import React from 'react';
       import { Button, Form, Input, Select, Checkbox, Radio, DatePicker, TimePicker } from "antd";
@@ -108,7 +125,7 @@ const FormCodeGenerator = ( ) => {
             ${componentCode.join("\n")}
             <Form.Item>
               <Button type="primary" htmlType="submit">
-                Submit
+                ${submitBtn}
               </Button>
             </Form.Item>
           </Form>
@@ -124,7 +141,7 @@ const FormCodeGenerator = ( ) => {
 
   const downloadCode = () => {
     const element = document.createElement("a");
-    const file = new Blob([componentCode], { type: 'text/plain' });
+    const file = new Blob([componentCode], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
     element.download = "generated_code.tsx";
     document.body.appendChild(element); // Required for this to work in Firefox
@@ -142,17 +159,6 @@ const FormCodeGenerator = ( ) => {
     }
   };
 
-  if (submitted) {
-    return (
-      <div className="flex justify-center w-full h-full items-center p-8">
-        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
-          <h1 className="text-2xl font-bold">Form submitted</h1>
-          <p className="text-muted-foreground">Thank you for submitting the form, you can close this page now.</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
       <button
@@ -161,37 +167,39 @@ const FormCodeGenerator = ( ) => {
         onClick={generateComponentCode}
       >
         Export Form
-      </button>
-
-      {isModalOpen && (
-        <div className="fixed inset-0 z-10 flex overflow-y-auto items-center w-full justify-center bg-gray-900 bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg max-w-md">
-            <h2 className="text-xl font-semibold mb-4">Generated Form Code</h2>
-            <code>{componentCode}</code>
-            <div className="flex justify-between">
-              <button
-                className="mt-4 bg-gray-900 text-white px-4 py-2 rounded-md hover:bg-gray-800"
-                onClick={copyToClipboard}
-              >
-                Copy Code
-              </button>
-              <button
-                className="mt-4 bg-gray-900 text-white px-4 py-2 rounded-md hover:bg-gray-800"
-                onClick={downloadCode}
-              >
-                Download Code
-              </button>
-              <button
-                className="mt-4 bg-gray-900 text-white px-4 py-2 rounded-md hover:bg-gray-800"
-                onClick={() => setIsModalOpen(false)}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
+      </button> 
+         
+      {isModalOpen && ( 
+            <Modal
+              title="Select Input Type"
+              visible={isModalOpen}
+              onCancel={() => setIsModalOpen(false)}
+              footer={[
+             <div className="flex flex-row justify-between w-full">
+                 <Button
+                  key="submit"
+                  type="primary"
+                  onClick={ 
+                    copyToClipboard }
+                >
+                  copy
+                </Button>,
+                <Button
+                  key="submit"
+                  type="primary"
+                  onClick={ 
+                    downloadCode }
+                >
+                  Download code
+                </Button>
+             </div>
+              ]}
+            >
+              <code className=" whitespace-pre-wrap text-sm text-gray-900">
+                {componentCode}
+              </code>
+            </Modal>  
       )}
- 
     </>
   );
 };
