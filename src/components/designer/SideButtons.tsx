8c2af0f99c14175df967669433e0c93e7c0c5191
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-08 16:56:17 +0000

    add selected button bg

--- src/components/designer/SideButtons.tsx
+++ src/components/designer/SideButtons.tsx
@@ -1,5 +1,5 @@
 "use client"; 
-import React, { useEffect, useState, useRef } from 'react'; 
+import React, { useState } from 'react'; 
 import FormCodeGenerator from "../forms/codeGenerator/FormCodeGenerator"; 
 import { Button } from 'antd';
 import { FiEdit } from "react-icons/fi";
@@ -7,39 +7,42 @@ import { MdOutlineDesignServices } from "react-icons/md";
 import { MdOutlinePreview } from "react-icons/md";
 
 
-export default function SideButtons({  onPreview }: {   onPreview: (value: boolean) => void }) {
-  const [preview, setPreview] = useState(false);
-
+export default function SideButtons({ onPreview }: { onPreview: (value: boolean) => void }) {
+  const [selectedButton, setSelectedButton] = useState<'preview' | 'field' | 'design'>('field');
 
   return (
     <div className="flex fixed pt-[80px] border-black border-r-[0.5px] bg-white w-[150px] h-[100vh]
     flex-col items-center justify-start top-0 gap-[23%]">
-      <div className="flex flex-col  gap-2 ">
-        <Button
-          className={`btn_header ${!preview ? "bg-zinc-100 text-zinc-800" : "bg-white"}`}
+      <div className="flex flex-col gap-2 ">
+           <Button
+          className={`btn_header ${selectedButton === 'field' ? "bg-blue-300 text-zinc-800" : "bg-white"}`}
+          onClick={() => {
+            onPreview(false);
+            setSelectedButton('field');
+          }}
+        >
+          <FiEdit/>
+          Field
+        </Button>
+            <Button
+          className={`btn_header ${selectedButton === 'preview' ? "bg-blue-300 text-zinc-800" : "bg-white"}`}
           onClick={() => {
             onPreview(true);
-            setPreview(true);
+            setSelectedButton('preview');
           }}
         >
           <MdOutlinePreview/>
           Preview
         </Button>
+ 
         <Button
-          className={`btn_header ${preview ? "bg-zinc-100 text-zinc-800" : "bg-white"}`}
+          className={`btn_header ${selectedButton === 'design' ? "bg-blue-300 text-zinc-800" : "bg-white"}`}
           onClick={() => {
-            onPreview(false);
-            setPreview(false);
+            // Add your logic for the design button here
+            setSelectedButton('design');
           }}
-        ><FiEdit/>
-          Field
-        </Button>
-       
-        <Button
-          className={`btn_header ${preview ? "bg-zinc-100 text-zinc-800" : "bg-white"}`}
-         
         > 
-          < MdOutlineDesignServices/>
+          <MdOutlineDesignServices/>
           Design
         </Button>
        
