083c6a50b6313baafa49071f3dc55ccb757ed8b7
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-09 12:01:29 +0000

    add published page

--- src/app/published/[id]/page.tsx
+++ src/app/published/[id]/page.tsx
@@ -1,7 +1,118 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import {  Button,  Form } from "antd";
+import { useSearchParams } from 'next/navigation' 
+import SelectPrev from "@/components/forms/previews/elementTypePreview/SelectPrev";
+import CheckBoxPrev from "@/components/forms/previews/elementTypePreview/CheckBoxPrev";
+import DatePrev from "@/components/forms/previews/elementTypePreview/DatePrev";
+import InputPrev from "@/components/forms/previews/elementTypePreview/InputPrev";
+import RadioPrev from "@/components/forms/previews/elementTypePreview/RadioPrev";
+ 
+export default function PublishFormPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const [form, setForm] = useState<Form >( );
+  const searchParams = useSearchParams()
 
-export default function page() {
+  const localStorageParam = searchParams.get('local')
+  const [loading, setLoading] = useState<boolean>(true);
+  const { id } = params;
+ 
+  const onFinish = (values: any) => {
+    console.log("Form submitted:", values);
+    setSubmitted(true);
+  };
+  const [submitted, setSubmitted] = useState(false); 
+ 
+  useEffect(() => {
+    fetchForm();
+  }, []);
+
+  const fetchForm = async () => { 
+    if (localStorageParam === 'true') {
+      try {
+        const forms = JSON.parse(localStorage.getItem("forms") || "[]");
+        const form = forms.find((form: Form) => form.id === parseInt(id));
+        if (form) {
+          setForm(form);
+        } else {
+        }
+      } catch (error) {
+        console.error("Error fetching form from local storage:", error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      try {
+        const response = await fetch(`/api/forms/${id}`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setForm(data.form);
+      } catch (error) {
+        console.error("Error fetching form:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+  if (!form) {
+    return <div>Form not found</div>;
+  }
   return (
-    <div>page</div>
-  )
+    <Form
+      onFinish={onFinish}
+      layout="vertical" // Set the layout to vertical
+    
+    >
+    <span className="text-md font-semibold"> {form!.content.length==0 && "No elements to Preview"}</span> 
+
+      {form!.content.map((element: any, index) => (
+        <div key={index}>
+          {["text", "number", "email", "password", "file", "textarea","url"].includes(
+            element.elementType.type
+          ) && <InputPrev element={element} />}
+
+          {["datetime-local", "date"].includes(element.elementType.type) && (
+            <DatePrev element={element} />
+          )}
+
+          {element.elementType.type === "time" && (
+         <DatePrev element={element} isTime={true} />
+          )}
+
+          {element.elementType.type === "select" && (
+            <SelectPrev element={element} />
+          )}
+          {element.elementType.type === "select_multiple" && (
+            <SelectPrev element={element} isMultiple={true} />
+         
+          )}
+          {element.elementType.type === "checkbox" && (
+            <CheckBoxPrev element={element} />
+          )}
+          {element.elementType.type === "radio" && (
+            <RadioPrev element={element} />
+          )}
+        </div>
+      ))}
+      <div className="flex justify-center pt-6">
+      (
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="h-10 font-bold py-2 px-4 w-1/2"
+          >
+            "submitBtn"
+          </Button>
+        ) 
+      </div>
+    </Form>
+  );
 }
+
