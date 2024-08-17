e57ad4a900b157147b9dd1e8ef96ab5412a7b709
deidine <deidinemycours@gmail.com>
2024-08-17 00:20:25 +0000

    add skelton to designer

--- src/components/skeletons/DesignSkeleton.tsx
+++ src/components/skeletons/DesignSkeleton.tsx
@@ -1,7 +1,7 @@
 import { Separator } from "@radix-ui/react-separator";
 import React from "react";
 import { Skeleton } from "../ui/skeleton";
-
+ 
 export default function DesignSkeleton() {
   return (
     <div
@@ -13,68 +13,46 @@ export default function DesignSkeleton() {
        border-r-black px-[80px]  h-full border-l-2 top-0 left-0 z-10"
       >
         <div className="flex flex-col pt-[120px] items-center  justify-between  ">
-          <Skeleton className="w-20 h-20" />
+          <div className="bg-white p-1 rounded-lg z-10 shadow-lg">
+            <Skeleton className="w-20 h-20  rounded-[5px]" />
+          </div>{" "}
+          <Separator
+            orientation="vertical"
+            decorative
+            className="h-3.5 border-neutral-100"
+          />{" "}
+          <div className="bg-white p-1 rounded-lg z-10 shadow-lg">
+            <Skeleton className="w-20 h-20  rounded-[5px]" />
+          </div>{" "}
+          <Separator
+            orientation="vertical"
+            decorative
+            className="h-3.5 border-neutral-100"
+          />{" "}
+          <div className="bg-white p-1 rounded-lg z-10 shadow-lg">
+            <Skeleton className="w-20 h-20  rounded-[5px]" />
+          </div>{" "}
           <Separator
             orientation="vertical"
             decorative
             className="h-3.5 border-neutral-100"
           />
-
-          <Skeleton className="w-20 h-20" />
-          <Separator
-            orientation="vertical"
-            decorative
-            className="h-3.5 border-neutral-100"
-          />
-
-          <Skeleton className="w-20 h-20" />
-          <Separator
-            orientation="vertical"
-            decorative
-            className="h-3.5 border-neutral-100"
-          />
-
-          <Skeleton className="w-20 h-20" />
         </div>
       </Skeleton>{" "}
       <div>
         <Skeleton className="h-[80px] w-ful z-20" />
       </div>
-
-      <div className="w-40 h-40 bg-black">deidein</div>
+      <div className=" mx-auto max-w-2xl  mt-3 border shadow rounded-xl w-1/2 h-[70%] ">
+        <Skeleton className=" h-full w-full  rounded-xl  " />
+      </div>
+      <div className="w-full fixed bottom-0 flex  
+      justify-end pr-[100px] pb-[50px] items-center right-0 shadow-lg   h-auto z-10 ">
+        <Skeleton className="w-40 h-14   fixed btn_header text-white flex items-center justify-around   bg-[#36b3fa]
+          font-[14px] text-2xl gap-4 " 
+          >
+          
+            </Skeleton>
+      </div>
     </div>
   );
-}
-
-{
-  /* 
-    <div className="mb-8 flex justify-between p-6">
-<Skeleton className="h-5 w-1/2" />
-<div className="flex w-1/3 gap-1.5">
-  <Skeleton className="size-5 shrink-0" />
-  <Skeleton className="h-5 w-full" />
-</div>
-</div>
-<div className="mt-2 p-6 pt-2">
-<Skeleton className="h-[75px] w-full" />
-<div className="mt-[52px] flex items-center justify-between gap-2">
-  <Skeleton className="h-3.5 w-1/4" />
-  <Separator
-    orientation="vertical"
-    decorative
-    className="h-3.5 border-neutral-100"
-  />
-  <Skeleton className="h-3.5 w-1/4" />
-  <Separator
-    orientation="vertical"
-    decorative
-    className="h-4 border-neutral-100"
-  />
-  <Skeleton className=" h-4 w-1/4" />
-</div>
-<Skeleton className="mt-5 h-4 w-1/2" />
-</div>
-<div className="p-2.5 pt-0">
-<Skeleton className="h-9 w-full rounded-[5px]" />
-</div> */
 }
