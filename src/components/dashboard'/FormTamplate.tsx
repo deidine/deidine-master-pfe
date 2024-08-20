bb616c09dc2f34a80d2d4de4033df336f511e85a
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-20 16:59:40 +0000

    add imgs

--- src/components/dashboard'/FormTamplate.tsx
+++ src/components/dashboard'/FormTamplate.tsx
@@ -2,16 +2,18 @@
 import React, { useEffect, useState } from "react";
 import { tamplateForms } from "@/data/tamplateForms";
 import PreviewFormsTamplates from "../forms/previews/PreviewFormsTamplates";
+import Image from "next/image";
 
 export default function FormTamplate() {
   return (
     //do cpature de ecran d'image
     <div>
-      {tamplateForms.map((form: Form) => (
+      {/* {tamplateForms.map((form: Form) => (
         <div key={form.id}>
           <PreviewFormsTamplates form={form} onFinish={() => {}} />
         </div>
-      ))}
+      ))} */}
+      <Image src="/formTamplateImgs/login.png" alt="image" width={500} height={500} />
     </div>
   );
 }
--- src/data/tamplateForms.ts
+++ src/data/tamplateForms.ts
@@ -1,175 +1,36 @@
  export const tamplateForms:Form[] = [
     {
-        id: 1,
-        title: "deidine",
-        content: [
+        "id": 1,
+        "title": "deidine",
+        "content": [
             {
-                id: "3008",
-                elementType: {
-                name: "jny9w0csxz",
-                    "type": "logo",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "9398",
-                elementType: {
-                name: "7z7vfn3t6tm",
-                    "type": "headingTitle",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder",
-                    "headingTitle": "Heading Title"
-                }
-            },
-            {
-                id: "126",
-                elementType: {
-                name: "s79ngjkun1e",
-                    "type": "select",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "options": [
-                        "Option 1",
-                        "Option 2"
-                    ],
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "2075",
-                elementType: {
-                name: "lplvdey2gr9",
+                "id": "1704",
+                "elementType": {
+                    "name": "yp67cf8nl",
                     "type": "text",
-                    "label": "Label",
+                    "label": "username",
                     "style": "h-10 rounded-lg border ",
                     "value": "",
                     "pattern": "",
-                    "required": false,
+                    "required": true,
                     "placeholder": "Placeholder"
                 }
             },
             {
-                id: "6470",
-                elementType: {
-                name: "lizg1lv6n8i",
-                    "type": "radio",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "options": [
-                        "Option 1",
-                        "Option 2"
-                    ],
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "521",
-                elementType: {
-                name: "tu8hmpqnd58",
-                    "type": "select",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "options": [
-                        "Option 1",
-                        "Option 2"
-                    ],
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "5659",
-                elementType: {
-                name: "3457rxe5kf7",
-                    "type": "date",
-                    "label": "Label",
+                "id": "6981",
+                "elementType": {
+                    "name": "cophv7e6jz",
+                    "type": "password",
+                    "label": "password",
                     "style": "h-10 rounded-lg border ",
                     "value": "",
                     "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "4826",
-                elementType: {
-                name: "b8gff9s2oi5",
-                    "type": "date",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "3632",
-                elementType: {
-                name: "scgbqp46bmj",
-                    "type": "radio",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "options": [
-                        "Option 1",
-                        "Option 2"
-                    ],
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "9276",
-                elementType: {
-                name: "o8k5jufs6vl",
-                    "type": "file",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "pattern": "",
-                    "required": false,
-                    "placeholder": "Placeholder"
-                }
-            },
-            {
-                id: "9358",
-                elementType: {
-                name: "0in0zjk2sfck",
-                    "type": "radio",
-                    "label": "Label",
-                    "style": "h-10 rounded-lg border ",
-                    "value": "",
-                    "options": [
-                        "Option 1",
-                        "Option 2"
-                    ],
-                    "pattern": "",
-                    "required": false,
+                    "required": true,
                     "placeholder": "Placeholder"
                 }
             }
         ],
-        "description": "Description",
-         
+        "description": "Description", 
         "style": {},
         "elementStyle": {},
         "buttonStyle": {}
