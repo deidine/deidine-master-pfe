4cf2405f7099ea63d3ee6e60a8088239fde66103
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-22 15:58:19 +0000

    deidein

--- src/app/(published)/page.tsx
+++ src/app/(published)/page.tsx
@@ -6,34 +6,43 @@ import Hero from "@/components/homePage/Hero";
 
 export default function Home() {
   return (
-    <div>
-<div className="flex flex-row justify-between items-center gap-4">
-<Hero/>
-<Hero/>
-
-</div>
-      <div className="p-2 flex flex-wrap gap-9">
-        <FeatureCard
-          icon={<CiSaveDown1 />}
-          title={"deiidne"}
-          description={
-            "we provide nice edit for input elemtn evry one con build wuiwly orm"
-          }
-        />
-        <FeatureCard
-          icon={<CiSaveDown1 />}
-          title={"deiidne"}
-          description={
-            "we provide nice edit for input elemtn evry one con build wuiwly orm"
-          }
-        />
-        <FeatureCard
-          icon={<CiSaveDown1 />}
-          title={"deiidne"}
-          description={
-            "we provide nice edit for input elemtn evry one con build wuiwly orm"
-          }
-        />
+    <div className="p-10">
+      <div className="flex flex-row justify-between items-center gap-4">
+        <Hero />
+    
+      </div>
+      <div>
+        <div className="font-semibold w-1/2  text-black mt-[1rem] mx-[1rem]">
+          <div className="py-10">
+            <span className="font-bold text-lg">
+            Main Features
+            </span>
+            <span className="block h-1 w-[20%] mt-1 bg-buttonColor  "></span>
+          </div>
+        </div>
+        <div className="p-2 flex flex-wrap gap-9">
+          <FeatureCard
+            icon={<CiSaveDown1 />}
+            title={"deiidne"}
+            description={
+              "we provide nice edit for input elemtn evry one con build wuiwly orm"
+            }
+          />
+          <FeatureCard
+            icon={<CiSaveDown1 />}
+            title={"deiidne"}
+            description={
+              "we provide nice edit for input elemtn evry one con build wuiwly orm"
+            }
+          />
+          <FeatureCard
+            icon={<CiSaveDown1 />}
+            title={"deiidne"}
+            description={
+              "we provide nice edit for input elemtn evry one con build wuiwly orm"
+            }
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/homePage/Hero.tsx
+++ src/components/homePage/Hero.tsx
@@ -4,28 +4,27 @@ import Image from "next/image"; // Import Image from next/image
 
 export default function Hero() {
   return (
-    <div>
-      <div className="font-semibold w-20  text-black mt-[1rem] mx-[1rem]">
-        <div>
-          deidiend
-          <span className="block h-1 mt-1 bg-buttonColor w-full "></span>
+    <div className="">
+      <div className="flex flex-row justify-between items-start pb-10">
+        <div className="wj-1/2">
+      <p className=" text-buttonColor font-bold text-2xl pb-4">Quick Form</p>
+       
+            <span className="text-black font-bold text-xl">
+              The easiest way to create forms people love to fill out
+            </span>
+          
+          <p className="w-[50%]">
+            Create online forms in the simplest and quickest way possible.
+            RapidForms offers the easiest and quickest way to create forms just
+            the way you want them, so you can focus on what matters most. Layout
+            alt 03 Ready-to-use templates Simplify your workflow and save time
+            with pre-designed forms that are easy to use and customize.
+          </p>
+        </div>
+        <div className="w-[80%]">
+          <Image src="/logo.jpg" alt="image" width={700} height={500} />
         </div>
       </div>
-      <span className=" text-buttonColor font-bold text-2xl">Quick Form</span>
-     
-     <div   className="w-1/2"> 
-      <div>
-        <span className="text-black font-bold text-xl">
-          The easiest way to create forms people love to fill out
-        </span>
-      </div>
-      <p>
-        Create online forms in the simplest and quickest way possible.
-        RapidForms offers the easiest and quickest way to create forms just the
-        way you want them, so you can focus on what matters most. Layout alt 03
-        Ready-to-use templates Simplify your workflow and save time with
-        pre-designed forms that are easy to use and customize.
-      </p></div>
     </div>
   );
 }
