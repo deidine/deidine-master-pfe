27995c020d8b96681180afd6c198e17f4aada23c
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-22 17:16:40 +0000

    ui

--- src/components/homePage/featureCard.tsx
+++ src/components/homePage/featureCard.tsx
@@ -4,7 +4,7 @@ export default function FeatureCard({ icon,title,description}:{
     title:string;
     description:string}) {
   return (
-    <div className="flex transform hover:scale-105 hover:shadow-2xl  flex-col cursor-pointer w-80 py-[20px] px-[25px] justify-between items-center gap-4 border-[1px] 
+    <div className="flex transform    flex-col cursor-pointer w-80 py-[20px] px-[25px] justify-between items-center gap-4 border-[1px] 
     shadow-sm border-gray-500 rounded-[16px] ">
       <div className="flex flex-row justify-start w-full items-center  gap-4">
         {icon}
