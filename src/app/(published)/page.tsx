0cb19c78f562554798b750e9d3d2b992da501bd3
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-22 15:32:49 +0000

    chnage home page

--- src/app/(published)/page.tsx
+++ src/app/(published)/page.tsx
@@ -1,20 +1,40 @@
 "use client";
- 
-import Content from "@/components/homePage/Content";
-import Features from "@/components/homePage/Features";
-import Footer from "@/components/homePage/Footer";
-import Hero from "@/components/homePage/Hero"; 
-import Support from "@/components/homePage/Support";
-  
-export default function Home() { 
+
+import { CiSaveDown1 } from "react-icons/ci";
+import FeatureCard from "@/components/homePage/featureCard";
+import Hero from "@/components/homePage/Hero";
+
+export default function Home() {
   return (
     <div>
-       <Hero/>
-      <Content/>
-      <Support/>
-      <Features/>
-      <Footer/>
+<div className="flex flex-row justify-between items-center gap-4">
+<Hero/>
+<Hero/>
 
+</div>
+      <div className="p-2 flex flex-wrap gap-9">
+        <FeatureCard
+          icon={<CiSaveDown1 />}
+          title={"deiidne"}
+          description={
+            "we provide nice edit for input elemtn evry one con build wuiwly orm"
+          }
+        />
+        <FeatureCard
+          icon={<CiSaveDown1 />}
+          title={"deiidne"}
+          description={
+            "we provide nice edit for input elemtn evry one con build wuiwly orm"
+          }
+        />
+        <FeatureCard
+          icon={<CiSaveDown1 />}
+          title={"deiidne"}
+          description={
+            "we provide nice edit for input elemtn evry one con build wuiwly orm"
+          }
+        />
+      </div>
     </div>
   );
 }
--- src/components/homePage/Hero.tsx
+++ src/components/homePage/Hero.tsx
@@ -3,125 +3,29 @@ import React, { useState } from "react";
 import Image from "next/image"; // Import Image from next/image
 
 export default function Hero() {
-  const [search, setSearch] = useState(false);
-  const [nav, setNav] = useState(false);
-  const Search = () => setSearch(true);
-  const Hide = () => setSearch(false);
-  const handleClick = () => setNav(!nav);
-
   return (
-    <div className="h-[90vh] w-full px-16 py-14 bg-slate-300 rounded-3xl drop-shadow-lg">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center justify-between gap-2">
-          <Image
-            src="/logo.jpg" // Use the Image component from next/image
-            alt="logo"
-            width={50} // Set the width according to your design
-            height={50} // Set the height according to your design
-            className="rounded-full" // Use only necessary classes
-          />
-          <h1 className="text-4xl">Form Builder</h1>
-        </div>
-
+    <div>
+      <div className="font-semibold w-20  text-black mt-[1rem] mx-[1rem]">
         <div>
-          <ul className="hidden md:flex items-center">
-            <li className="mx-4 md:mx-2 lg:text-lg">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="mx-4 md:mx-2 lg:text-lg">
-              <Link href="/forms">Forms</Link>
-            </li>
-            <li className="mx-4 md:mx-2 lg:text-lg">
-              <a href="#about">About</a>
-            </li>
-            <li className="mx-4 md:mx-2 lg:text-lg">
-              <a href="#documentation">Documentation</a>
-            </li>
-            <li className="mx-4">
-              <div className="w-7" onClick={Search} />
-            </li>
-          </ul>
-        </div>
-
-        <div className="hidden md:flex">
-          <Link href="/login">
-            <button className="lg:px-8 lg:py-2 rounded-md md:mx-2 md:px-2 md:py-1 lg:text-lg md:text-sm">
-              Login
-            </button>
-          </Link>
-          <Link href="/login">
-            <button className="border border-slate-500 lg:px-8 md:px-3 md:py-2 rounded-md mx-2 lg:text-lg md:text-sm bg-mainColor text-white hover:bg-sky-100 hover:text-black font-bold">
-              Register
-            </button>
-          </Link>
-        </div>
-
-        <div onClick={handleClick}>
-          {!nav ? (
-            <div className="w-7 md:hidden" />
-          ) : (
-            <div className="w-7 md:hidden" />
-          )}
+          deidiend
+          <span className="block h-1 mt-1 bg-buttonColor w-full "></span>
         </div>
       </div>
-
-      <ul
-        className={
-          !nav
-            ? "hidden"
-            : "absolute w-[80%] mx-auto md:hidden items-center my-5 bg-slate-50 px-8 rounded-xl"
-        }
-      >
-        <li className="mx-4 py-4">
-          <div className="w-7" onClick={Search} />
-        </li>
-
-        <li className="mx-4 py-3 border-b-2 border-slate-200">Home</li>
-        <li className="mx-4 py-3 border-b-2 border-slate-200">About</li>
-        <li className="mx-4 py-3 border-b-2 border-slate-200">Documentation</li>
-        <li className="mx-4 py-3 border-b-2 border-slate-200">Product</li>
-        <li className="py-3">
-          <div className="md:hidden">
-            <button className="md:hidden w-full rounded-lg py-3 my-3 border-2 border-indigo-600 text-xl font-bold">
-              Login
-            </button>
-            <button className="md:hidden w-full rounded-lg py-3 my-2 bg-mainColor text-white text-xl font-bold hover:bg-sky-100 hover:text-black">
-              Register
-            </button>
-          </div>
-        </li>
-      </ul>
-
-      <div
-        className={
-          search
-            ? "absolute w-full flex items-start justify-center h-screen bg-gradient-to-r from-slate-600 to-transparent top-0 left-0"
-            : "hidden"
-        }
-      >
-        <form action="" className="my-16 absolute top-1/4 flex">
-          <input
-            type="text"
-            placeholder="Search"
-            className="py-5 px-16 rounded-3xl"
-          />
-          <div className="w-7 mx-3" onClick={Hide} />
-        </form>
+      <span className=" text-buttonColor font-bold text-2xl">Quick Form</span>
+     
+     <div   className="w-1/2"> 
+      <div>
+        <span className="text-black font-bold text-xl">
+          The easiest way to create forms people love to fill out
+        </span>
       </div>
-
-      <div className="w-full h-full flex items-center justify-center flex-wrap py-5">
-        <div>
-          <h1 className="text-2xl font-bold">
-            <span className="text-6xl font-bold text-blue-800 py-3">Welcome to</span> Form Builder
-          </h1>
-          <p className="text-2xl font-medium py-3">
-            Simplify the way you design forms with our intuitive drag-and-drop interface.
-          </p>
-          <button className="bg-mainColor text-white rounded-lg py-3 my-2 px-3 font-bold hover:bg-sky-100 hover:text-black">
-            Get Started
-          </button>
-        </div>
-      </div>
+      <p>
+        Create online forms in the simplest and quickest way possible.
+        RapidForms offers the easiest and quickest way to create forms just the
+        way you want them, so you can focus on what matters most. Layout alt 03
+        Ready-to-use templates Simplify your workflow and save time with
+        pre-designed forms that are easy to use and customize.
+      </p></div>
     </div>
   );
 }
