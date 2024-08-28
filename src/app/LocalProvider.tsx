5777d346170774456511b701faeb7f65f06999eb
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-28 14:55:59 +0000

    add lang

--- src/app/LocalProvider.tsx
+++ src/app/LocalProvider.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { LocaleProvider } from "@douyinfe/semi-ui";
-import en_US from "@douyinfe/semi-ui/lib/es/locale/source/en_US";
-
+import React, { useEffect, useState } from "react"; 
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n';
 import "./i18n/i18n";
 
 export default function LocalProvider({ children }: { children: React.ReactNode }) {
@@ -17,5 +16,5 @@ export default function LocalProvider({ children }: { children: React.ReactNode 
     return null; // or a loading spinner, skeleton UI, etc.
   }
 
-  return <LocaleProvider locale={en_US}>{children}</LocaleProvider>;
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
